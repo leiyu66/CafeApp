bff840d29114f9718a3f52c527276e502c80e974
leiyu66 <lei.yu@microsoft.com>
2016-12-03 00:33:57 -0500

    Implemented Open Tab event

--- src/CafeApp.Core.Tests/Library1.fs
+++
@@ -1,8 +0,0 @@
-namespace CafeApp.Core.Tests
-open CafeApp.Core
-open NUnit.Framework
-module UnitTests =
-  [<Test>]
-  let ``Sample Test``() =
-    let class1 = new Class1()
-    Assert.AreEqual("F#", class1.X)
--- src/CafeApp.Core/Library1.fs
+++
@@ -1,4 +0,0 @@
-namespace CafeApp.Core
-
-type Class1() = 
-    member this.X = "F#"
